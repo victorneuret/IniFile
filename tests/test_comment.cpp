efd6f7fed9e31d39a3e551a3f1c0f9b17def82bf
Victor Neuret <victor.neuret@bluenove.com>
2018-09-26 15:08:04 +0200

    Add tests for comments (line and inline)

---
+++ tests/test_comment.cpp
@@ -0,0 +1,84 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+
+#include <criterion/criterion.h>
+
+#include "IniParser.hpp"
+#include "tests.hpp"
+
+Test(comment, lineCommentOne)
+{
+	std::string fileName = "testLineCommentOne.ini";
+	std::ofstream testFile(fileName);
+	MAP_DEFINITION map;
+
+    testFile << "# first line comment" << std::endl;
+	testFile << "firstName = John" << std::endl;
+	testFile << "# This comment is awesome!" << std::endl;
+    testFile << "LastName = Doe" << std::endl;
+	IniParser parser(fileName);
+	map = parser.getMap();
+    cr_assert_eq(map.count("global"), 1);
+	cr_assert_eq(map["global"].size(), 2);
+	cr_assert_eq(map["global"]["firstName"], "John");
+	cr_assert_eq(map["global"]["lastName"], "Doe");
+	remove(fileName.c_str());
+}
+
+Test(comment, lineCommentTwo)
+{
+	std::string fileName = "testLineCommentTwo.ini";
+	std::ofstream testFile(fileName);
+	MAP_DEFINITION map;
+
+    testFile << "; first line comment" << std::endl;
+	testFile << "firstName = John" << std::endl;
+	testFile << "; This comment is awesome!" << std::endl;
+    testFile << "LastName = Doe" << std::endl;
+	IniParser parser(fileName);
+	map = parser.getMap();
+    cr_assert_eq(map.count("global"), 1);
+	cr_assert_eq(map["global"].size(), 2);
+	cr_assert_eq(map["global"]["firstName"], "John");
+	cr_assert_eq(map["global"]["lastName"], "Doe");
+	remove(fileName.c_str());
+}
+
+Test(comment, inlineCommentOne)
+{
+	std::string fileName = "testInlineCommentOne.ini";
+	std::ofstream testFile(fileName);
+	MAP_DEFINITION map;
+
+    testFile << "firstName = John #Doe" << std::endl;
+	testFile << "# This comment is awesome!" << std::endl;
+    testFile << "LastName = Doe      #    Comment" << std::endl;
+	IniParser parser(fileName);
+	map = parser.getMap();
+    cr_assert_eq(map.count("global"), 1);
+	cr_assert_eq(map["global"].size(), 2);
+	cr_assert_eq(map["global"]["firstName"], "John");
+	cr_assert_eq(map["global"]["lastName"], "Doe");
+	remove(fileName.c_str());
+}
+
+Test(comment, inlineCommentTwo)
+{
+	std::string fileName = "testInlineCommentTwo.ini";
+	std::ofstream testFile(fileName);
+	MAP_DEFINITION map;
+
+    testFile << "firstName = John ;Doe" << std::endl;
+	testFile << "# This comment is awesome!" << std::endl;
+    testFile << "LastName = Doe      ;    Comment" << std::endl;
+	IniParser parser(fileName);
+	map = parser.getMap();
+    cr_assert_eq(map.count("global"), 1);
+	cr_assert_eq(map["global"].size(), 2);
+	cr_assert_eq(map["global"]["firstName"], "John");
+	cr_assert_eq(map["global"]["lastName"], "Doe");
+	remove(fileName.c_str());
+}
