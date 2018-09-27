e06a045b159c04be7720ff381d8f249b9dadcc5b
rasorn <victor.neuret@epitech.eu>
2018-09-28 00:57:04 +0200

    Add a unit test for multiple sections

--- tests/TestSection.cpp
+++ tests/TestSection.cpp
@@ -24,3 +24,24 @@ Test(section, section)
 	cr_assert_eq(map["Section"].size(), 2);
 	remove(fileName.c_str());
 }
+
+Test(section, multipleSection)
+{
+	std::string fileName = "testMultipleSection.ini";
+	std::ofstream testFile(fileName);
+	MAP_DEFINITION map;
+
+	testFile << "[Section]" << std::endl;
+	testFile << "firstName = John" << std::endl;
+	testFile << "lastName = Doe" << std::endl;
+	testFile << "[Section2]" << std::endl;
+	testFile << "age = 42" << std::endl;
+	testFile << "fruit = Banana" << std::endl;
+	IniParser parser(fileName);
+	map = parser.getMap();
+	cr_assert_eq(map.count("Section"), 1);
+	cr_assert_eq(map["Section"].size(), 2);
+	cr_assert_eq(map.count("Section2"), 1);
+	cr_assert_eq(map["Section2"].size(), 2);
+	remove(fileName.c_str());
+}
